573c0a793bfa440e7adce9db32601f804853e505
Arabica App <arabica@example.com>
2025-12-02 19:50:10 +0000

    Исправлен html_stub: добавлены statusText, abort, правильные сигнатуры методов

--- lib/html_stub.dart
+++ lib/html_stub.dart
@@ -6,21 +6,26 @@ import 'dart:async';
 class HttpRequest {
   int? status;
   String? responseText;
+  String? statusText;
   
-  void open(String method, String url, bool async) {}
+  void open(String method, String url, [bool? async, String? user, String? password]) {}
   void setRequestHeader(String name, String value) {}
   void send([dynamic data]) {}
+  void abort() {}
   
   Stream<dynamic> get onLoad => const Stream<dynamic>.empty();
   Stream<dynamic> get onError => const Stream<dynamic>.empty();
 }
 
 class FormData {
-  void appendBlob(String name, Blob blob, String fileName) {}
+  void appendBlob(String name, Blob blob, [String? fileName]) {}
   void append(String name, String value) {}
 }
 
 class Blob {
-  Blob(List<int> data, String type);
+  final List<int> data;
+  final String type;
+  
+  Blob(this.data, this.type);
 }
 
