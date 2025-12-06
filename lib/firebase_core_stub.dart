b33cfda4a6bef93a635ce8e8e0af6a2d3b7606c3
Arabica App <arabica@example.com>
2025-12-06 10:33:22 +0000

    Добавлен метод app() в firebase_core_stub.dart

--- lib/firebase_core_stub.dart
+++ lib/firebase_core_stub.dart
@@ -1,10 +1,20 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+/// Заглушка для Firebase App
+class FirebaseApp {
+  final String name;
+  FirebaseApp(this.name);
+}
+
 /// Заглушка для firebase_core на веб-платформе
 class Firebase {
   static Future<void> initializeApp() async {
     // Заглушка - ничего не делает на веб
     return;
   }
+  
+  /// Заглушка для получения Firebase App
+  static FirebaseApp app([String name = '[DEFAULT]']) {
+    throw UnsupportedError('Firebase App не доступен на этой платформе');
+  }
 }
-
-
-
