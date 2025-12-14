03f89542f34b09b4dd892717a232bb4bbb50ecad
Arabica App <arabica@example.com>
2025-12-14 19:34:44 +0000

    Исправлен импорт SharedPreferences в send_message_dialog.dart

--- lib/send_message_dialog.dart
+++ lib/send_message_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
